fdfb5d6c634659ac35470f8115a4023dab81dbae
beadoer1 <beadoer1@gmail.com>
2021-03-19 00:25:02 +0900

    21.03.18 알고리즘 풀이 업데이트

--- 20210311/7576.py
+++ 20210311/7576.py
@@ -20,58 +20,139 @@
 # 여러분은 토마토가 모두 익을 때까지의 최소 날짜를 출력해야 한다. 
 # 만약, 저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고, 토마토가 모두 익지는 못하는 상황이면 -1을 출력해야 한다.
 
-# 풀이
-import sys
+# 풀이 2: 다른 사람 풀이 참고 후 재작성(21.03.18)
+from collections import deque # BFS 활용을 위해 queue 사용
+M,N = map(int,input().split())
+tot_boxes = [list(map(int,input().split())) for _ in range(N)]
 
-tomatoes = {
+to_visit = deque([])
+for i in range(N): # 현재 익어있는 토마토를 to_visit에 넣어준다.(들러서 주변 애들 익힐 예정)
+    for j in range(M):
+        if tot_boxes[i][j] == 1:
+            to_visit.append([i,j])
 
-}
-ready = []
-empty = []
-def days_tomatoes_all_ready():
-    to_ready = ready
-    count = 0
-    while to_ready:
-        one_cycle = len(to_ready)
-        for tom in range(one_cycle):            
-            x,y = map(int,to_ready.pop(0).split('_'))
-            line_tot[x][y] = 1
-            if line_tot[x+1][y] != None and line_tot[x+1][y] == 0:
-                to_ready.append(str(x+1)+"_"+str(y))
-            if line_tot[x-1][y] != None and line_tot[x-1][y] == 0:
-                to_ready.append(str(x-1)+"_"+str(y))
-            if line_tot[x][y+1] != None and line_tot[x][y+1] == 0:
-                to_ready.append(str(x)+"_"+str(y+1))
-            if line_tot[x][y-1] != None and line_tot[x][y-1] == 0:
-                to_ready.append(str(x)+"_"+str(y-1))    
-        count += 1
-    return count    
+dx = [1,-1,0,0] # 좌우상하에 있는 토마토들을 확인하기 위한 좌표 +,- 설정
+dy = [0,0,1,-1]
+
+while to_visit:
+    row, col = to_visit.popleft() # 이와 같은 방식으로 [row,col] 값을 빼낼 수 있다.
+
+    for i in range(4): # 좌우 상하에 대한 반복문(코드가 엄청 보기 좋아진다.)
+        _row = row + dx[i]
+        _col = col + dy[i]
+
+        if 0<=_row < N and 0<=_col < M and tot_boxes[_row][_col] == 0: # indexError 방지를 위함과 덜익은 토마토 확인 조건문
+            tot_boxes[_row][_col] = tot_boxes[row][col] + 1 # 덜 익어있으면 익은 토마토보다 +1하여 저장한다.(단계를 구분하기 위함)
+            to_visit.append([_row,_col]) # to_visit에 넣어줘 다음 단계를 밟을 수 있게 함
+
+result = -2 # 단계를 구분할 때 비교값. 토마토가 들어있지 않은 경우 -1 이므로 -2를 넣어줌
+check_tot = False # 아직 덜 익은 토마토가 박스에 남아있는지 확인하기 위함
+for i in tot_boxes: # 토마토 박스에 덜익은 토마토가 있는지 확인하고, 없을 시 최대 단계가 몇단계인지 확인
+    for j in i:
+        if j == 0:
+            check_tot = True
+        elif j > result:
+            result = j
+       
+
+if check_tot == True: # 덜 익은 토마토(0)가 있는 경우 -1 반환
+    print(-1)
+elif result == -1: # 모든 수가 -1인 경우를 대비하여 별도 조건문 만들어 줌
+    print(0)
+else: # 1단계 : 2 , 2단계 : 3 , ... 순서로 들어가므로 1 빼주고 출력
+    print(result-1)
+
+
+# 풀이 1: 실패(정확한 방법에 대해 아예 감을 잡지 못함)
+# import sys
+
+# tomatoes = {
+
+# }
+# ready = []
+# empty = []
+# def days_tomatoes_all_ready():
+#     to_ready = ready
+#     count = 0
+#     while to_ready:
+#         one_cycle = len(to_ready)
+#         for tom in range(one_cycle):            
+#             x,y = map(int,to_ready.pop(0).split('_'))
+#             line_tot[x][y] = 1
+#             if line_tot[x+1][y] != None and line_tot[x+1][y] == 0:
+#                 to_ready.append(str(x+1)+"_"+str(y))
+#             if line_tot[x-1][y] != None and line_tot[x-1][y] == 0:
+#                 to_ready.append(str(x-1)+"_"+str(y))
+#             if line_tot[x][y+1] != None and line_tot[x][y+1] == 0:
+#                 to_ready.append(str(x)+"_"+str(y+1))
+#             if line_tot[x][y-1] != None and line_tot[x][y-1] == 0:
+#                 to_ready.append(str(x)+"_"+str(y-1))    
+#         count += 1
+#     return count    
             
-M,N = map(int,sys.stdin.readline().split())
-line_tot = []
-for i in range(N):
-    line = list(map(int,sys.stdin.readline().split()))
-    line_tot.append(line)
-for i in range(M):
-    for j in range(N):
-        if line_tot[i][j] == 1:
-            ready.append(str(i)+"_"+str(j))
-        elif line_tot[i][j] == -1:
-            empty.append(str(i)+"_"+str(j))
+# M,N = map(int,sys.stdin.readline().split())
+# line_tot = []
+# for i in range(N):
+#     line = list(map(int,sys.stdin.readline().split()))
+#     line_tot.append(line)
+# for i in range(M):
+#     for j in range(N):
+#         if line_tot[i][j] == 1:
+#             ready.append(str(i)+"_"+str(j))
+#         elif line_tot[i][j] == -1:
+#             empty.append(str(i)+"_"+str(j))
 
-print(days_tomatoes_all_ready())
+# print(days_tomatoes_all_ready())
 
+# 풀이 : 다른 사람 풀이(이를 통해 BFS를 이해해보자.) ↓↓↓아래 코드의 과정↓↓↓
+# 6 4
+# 0 -1 0 0      0 -1 0 0      0 -1 0 0      0 -1 0 4      0 -1 5 4  결과 : 0이 있네.-> True -> -1 출력
+# -1 0 0 0  ->  -1 0 0 0  ->  -1 0 0 3  ->  -1 0 4 3  ->  -1 5 4 3
+# 0 0 0 0       0 0 0 2       0 0 3 2       0 4 3 2       5 4 3 2
+# 0 0 0 1       0 0 2 1       0 3 2 1       4 3 2 1       4 3 2 1
 
+# from collections import deque # BFS 에 유리한 queue 사용을 위한 collections import
 
+# M, N = map(int, input().split()) # 입력을 받는다. M : 상자의 가로 칸 수, N : 상자의 세로 칸 수
+# tots = [list(map(int, input().split())) for _ in range(N)] # 각 줄의 상자들을 입력 받는다.
+# queue = deque() # to_visit 의 개념으로 현재 이미 익어있는 토마토의 좌표를 먼저 저장하고 향후 새로 익은 토마토를 추가로 넣어줄 용도
 
+# for i in range(5): # 이미 익어있는 토마토들에 우선 방문하기 위해 좌표 list [i,j]를 append 해준다.
+#     for j in range(5):
+#         if tots[i][j] == 1:
+#             queue.append([i, j])
 
-            # if line_tot[x+1][y] not in ready or line_tot[x+1][y] not in empty or line_tot[x+1][y] not in to_ready:
-            #     to_ready.append(str(x+1)+"_"+str(y))
-            # if line_tot[x-1][y] not in ready or line_tot[x-1][y] not in empty or line_tot[x+1][y] not in to_ready:
-            #     to_ready.append(str(x-1)+"_"+str(y))
-            # if line_tot[x][y+1] not in ready or line_tot[x][y+1] not in empty or line_tot[x+1][y] not in to_ready:
-            #     to_ready.append(str(x)+"_"+str(y+1))
-            # if line_tot[x][y-1] not in ready or line_tot[x][y-1] not in empty or line_tot[x+1][y] not in to_ready:
-            #     to_ready.append(str(x)+"_"+str(y-1))
-            # else:
-            #     continue
+# # 상하좌우 체크를 위한 x,y 좌표에 더해줄 값 list ★★★★★, BFS 하려면 모르면 안될 내용일듯.(코드가 엄청 깔끔해진다.)         
+# dx = [1, -1, 0, 0]
+# dy = [0, 0, 1, -1]
+
+# # 여기부터 bfs
+# while queue:
+#     row, col = queue.popleft() # 익어있는(1) 토마토에 방문하기 위해 popleft(). ★list [row,col]을 좌측과 같이 받을 수 있다.★
+    
+#     for k in range(4):
+#         _row = row + dy[k]     # 익어있는 토마토 상하좌우(dx,dy)를 확인하기 위한 변수 _row, _col
+#         _col = col + dx[k]
+        
+#         # 안익은게 있다면, 익은 것으로 바꿔준다.
+#         if 0 <= _row < N and 0 <= _col < M and tots[_row][_col] == 0: # indexError 방지를 위한 조건 과 안익은 토마토인지 확인하는 조건
+#             tots[_row][_col] = tots[row][col] + 1 # 안 익은 토마토를 익은 토마토로 바꿔 준다.(단계 표현을 위해 + 1 해준다. 첫 날:2,둘 째 날:3, ..)
+#             queue.append([_row, _col]) # 이제 익은 토마토를 향후에 방문하기 위해 큐에 넣어 준다.
+# # bfs 끝------
+
+# result = -2 # -1이 존재해서 -2 값으로 비교
+
+# check_tot = False # 안익은게 있는지 체크하기 위한 bool 변수
+
+# for i in tots: # tots list 안에 있는 list들의 변수 i
+#     for j in i: # tots list 안에 i list 안에 원소들의 변수 j
+#         if(j == 0): # 남아있는 원소들 중 0이 있으면
+#             check_tot = True # True로 변경
+#         result = max(result, j) # j가 1인 경우에는??
+        
+# if check_tot: # 남아있는 원소들 중에 0이 있는 경우 
+#     print(-1)
+# elif result == -1: # 처음부터 모두 -1일 때를 대비한 조건
+#     print(0)
+# else: 
+#     print(result - 1) # 모두 익은 상황이면 제일 큰 숫자에서 -1 (첫 날 모두 익어있는 상태라면 1이 최대값이므로, 첫 날 익은 토마토에 숫자 2 넣어주므로)
---
+++ 20210318/2667.py
@@ -0,0 +1,106 @@
+# 문제
+# <그림 1>과 같이 정사각형 모양의 지도가 있다. 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 
+# 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 
+# 여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 
+# 대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 
+# 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+# 입력
+# 첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
+# 출력
+# 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
+# 입력 예      출력 예
+# 7           3
+# 0110100     7
+# 0110101     8
+# 1110101     9
+# 0000111
+# 0100000
+# 0111110
+# 0111000
+
+# 풀이 1: 단지를 구분해야 하므로 DFS가 더 어울리는 문제라고 생각한다.
+# 예상 순서 : 1을 찾는다 -> 첫 번째 찾은 1부터 DFS로 따라 들어가며 숫자를 2로 바꾼다.
+#         -> 다 바꿨다면 다시 1을 찾는다 -> 이번엔 3으로 바꿔본다. -> 더이상 1이 없을 때까지 반복
+import sys
+N = int(sys.stdin.readline())
+tot_list = [list(map(int,input())) for _ in range(N)]
+houses = [] # to_visit의 역할
+
+complex_num = 2 # 0,1 로 구성되어 있으니 2부터 번호 메겨줌
+for i in range(N):
+    for j in range(N):
+        if tot_list[i][j] == 1:
+            houses.append([i,j])
+            # DFS 시작
+            while houses:
+                # 첫번째 방문할 곳을 pop() 하고 그 숫자를 2로 바꿔줌
+                row,col = houses.pop()
+                tot_list[row][col] = complex_num
+                # 방문하는 집의 좌우상하 구현을 준비
+                dx = [1,-1,0,0] # [x+1][y],[x-1][y],[x][y+1],[x][y-1] 조작을 위함
+                dy = [0,0,1,-1]
+                for k in range(4):
+                    _row = row + dx[k]
+                    _col = col + dy[k]
+                    if 0<=_row < N and 0<=_col < N and tot_list[_row][_col] == 1:
+                        houses.append([_row,_col])
+            complex_num+=1 # 단지 별로 숫자를 달리 해주기 위해 +=1 을 해준다.
+            # DFS 끝~~
+    
+result = {} # 단지 별 집의 빈도수를 기록하기 위한 dictionary
+count = 0
+for i in tot_list:
+    for j in i:
+        if j not in result and j != 0:
+            result[j] = 1
+        elif j in result and j != 0:
+            result[j] += 1
+fre_list = sorted(result.values()) # 집의 수 오름차순 출력을 위한 정렬
+print(len(result)) # 총 단지 수 출력
+for i in fre_list: # 단지 별 집의 수 출력
+    print(i)
+
+# 풀이 1: 단지를 구분해야 하므로 DFS가 더 어울리는 문제라고 생각한다.
+# 예상 순서 : 1을 찾는다 -> 첫 번째 찾은 1부터 DFS로 따라 들어가며 숫자를 2로 바꾼다.
+#         -> 다 바꿨다면 다시 1을 찾는다 -> 이번엔 3으로 바꿔본다. -> 더이상 1이 없을 때까지 반복
+# 문제점 : 단지 수가 줄의 수를 넘어가는 부분을 캐치 못함, 2중 for 문에서 j 가 한 줄에 여러개인 경우 캐치 못함
+# import sys
+# N = int(sys.stdin.readline())
+# tot_list = [list(map(int,input())) for _ in range(N)]
+# # 2차원 배열 생성
+# houses = [] # to_visit의 역할
+
+# complex_num = 2 # 0,1 로 구성되어 있으니 2부터 번호 메겨줌
+# for i in range(N):
+#     for j in range(N):
+#         if tot_list[i][j] == 1:
+#             houses.append([i,j])
+#             # break # 하나를 찾으면 아래에서 DFS를 실행하기 위해 break를 걸어줌 # 반례 : 줄의 길이를 넘어가는 수의 단지일 경우 i를 넘어가게 되어 for 문을 끊으면 안된다.
+#     # DFS 시작
+#     while houses:
+#         # 첫번째 방문할 곳을 pop() 하고 그 숫자를 2로 바꿔줌
+#         row,col = houses.pop()
+#         tot_list[row][col] = complex_num
+#         # 방문하는 집의 좌우상하 구현을 준비
+#         dx = [1,-1,0,0] # [x+1][y],[x-1][y],[x][y+1],[x][y-1] 조작을 위함
+#         dy = [0,0,1,-1]
+#         for k in range(4):
+#             _row = row + dx[k]
+#             _col = col + dy[k]
+#             if 0<=_row and _row< N and 0<=_col and _col < N and tot_list[_row][_col] == 1:
+#                 houses.append([_row,_col])
+#     complex_num+=1 # 단지 별로 숫자를 달리 해주기 위해 +=1 을 해준다.
+#             # DFS 끝~~
+    
+# result = {} # 단지 별 집의 빈도수를 기록하기 위한 dictionary
+# count = 0
+# for i in tot_list:
+#     for j in i:
+#         if j not in result and j != 0:
+#             result[j] = 1
+#         elif j in result and j != 0:
+#             result[j] += 1
+# fre_list = sorted(result.values()) # 집의 수 오름차순 출력을 위한 정렬
+# print(len(result)) # 총 단지 수 출력
+# for i in fre_list: # 단지 별 집의 수 출력
+#     print(i)
